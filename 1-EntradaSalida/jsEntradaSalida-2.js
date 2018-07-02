119c2c0971753df19cbfe657894b006657c5d7bf
alpuinschunk <40763575+alpuinschunk@users.noreply.github.com>
2018-07-02 12:06:57 -0300

    Ejer 1 2 3
    Los tres primeros

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,20 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
+	//comentario de una linea
+	//desde w3school
+	//var person = prompt("Please enter your name", "Harry Potter");
 	
+	/*
+	var person = prompt("Please enter your name", "Harry Potter");
+	var person = prompt("Please enter your name", "Harry Potter");
+	var person = prompt("Please enter your name", "Harry Potter");
+	*/
+
+	var nombre;
+	nombre=prompt("Por favor ingrese su nombre:","nombre")
+	alert("El nombre es: "+nombre);
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -1,8 +1,13 @@
 /*Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	/*
+	alert("Ejercicio 3");
+	console.log("Ejercicio 3");
+	*/
+	var nombre;
+	nombre=document.getElementById('elNombre').value;
+	//get tab shift i
+	alert(nombre);
 }
-
-
