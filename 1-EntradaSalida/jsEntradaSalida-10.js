5791e2c69df609952ac0266adca6983880b13602
alpuinschunk <40763575+alpuinschunk@users.noreply.github.com>
2018-07-04 15:50:50 -0300

    Ejercicio 10
    Rehecho

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,13 +4,12 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	var sueldo;
+	var importe
 	var resultado;
 
-	sueldo=document.getElementById('sueldo').value;
-	sueldo=parseInt(sueldo);
+	importe=document.getElementById('importe').value;
+	importe=parseInt(importe);
 
-	resultado=sueldo - sueldo*0.25;
-
+	resultado=importe-(importe*0.25);
 	document.getElementById('resultado').value=resultado;
 }
