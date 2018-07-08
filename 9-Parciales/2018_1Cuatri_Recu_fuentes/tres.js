abae63188dead78a9ad93291f6bda6011fe610c5
alpuinschunk <40763575+alpuinschunk@users.noreply.github.com>
2018-07-08 11:33:35 -0300

    Ejercicios 1, 2 y 3
    Ejercicios de recuperatorio

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var iva;
+	var precioFinal;
 
+	precio=prompt("Ingrese el precio");
+	descuento=prompt("Ingrese el porcentaje de descuento");
+
+	descuento=(descuento*precio)/100
+	precio=precio-descuento;
+	iva=(21*precio)/100;
+
+	alert("El descuento en dinero es de $"+descuento+". El precio con el descuento es de $"+precio+". El IVA es de $"+iva);
+
+	precioFinal=precio+iva;
+
+	document.getElementById('elPrecioFinal').value=precioFinal;
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,16 @@
 
 function mostrar()
 {
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
 
+	base=prompt("Ingrese la base");
+	altura=prompt("Ingrese la altura");
+
+	superficie=(base*altura)/2;
+	perimetro=base*3
+
+	alert("La superficie del triangulo es "+superficie+", y el perimetro es "+perimetro);
 }
