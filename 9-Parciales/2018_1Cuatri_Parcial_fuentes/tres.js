b6da05d1830bd05dfeb09cf3bfadac6aea9c1dcd
alpuinschunk <40763575+alpuinschunk@users.noreply.github.com>
2018-07-08 11:34:54 -0300

    Ejercicios 1, 2 y 3
    Ejercicios de parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,14 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var precioFinal;
 
+	precio=prompt("Ingrese el precio");
+	descuento=prompt("Ingrese el porcentaje de descuento");
+
+	descuento=(descuento*precio)/100;
+	precioFinal=precio-descuento;
+
+	document.getElementById('elPrecioFinal').value=precioFinal;
 }
