94b83d211980bdf9742b740cd4586459645f75b3
alpuinschunk <40763575+alpuinschunk@users.noreply.github.com>
2018-07-23 12:16:50 -0300

    Ejercicios hechos

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,7 +1,45 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
+	console.log(mesDelAño);
+
+	switch(mesDelAño)
+	{
+		case "Enero":
+			alert("¡Que comiences bien el año!");
+			break;
+
+		case "Marzo":
+			alert("¡A clases!");
+			break;
+
+		case "Julio":
+			alert("¡Se vienen las vacaciones!");
+			break;
+
+		case "Diciembre":
+			alert("¡Felices Fiestas!");
+			break;
+	}
+
+	/*if(mes=="Enero")
+	{
+
+	}
+	if(mes=="Febrero")
+	{
+	Para evitar el if
+	}
+	switch(mes)
+	{
+		case "Febrero":
+			alert("verano");
+			break;
+		case "Enero":
+			alert("verano");
+			break;
+	}*/
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -5,7 +5,24 @@ var mesDelAño = document.getElementById('mes').value;
 
 alert (mesDelAño);
 
-
-
+	switch(mesDelAño)
+	{
+		case "Julio":
+		
+		case "Agosto":
+			alert("Abrigate que hace frio");
+			break;
+		
+		case "Enero":
+		case "Febrero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+			alert("Falta para el invierno");
+			break;
+		default:
+			alert("Ya pasamos el frio, ahora calor");
+	}
 
 }//FIN DE LA FUNCIÓN
