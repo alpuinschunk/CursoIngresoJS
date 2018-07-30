5135d2dfd176c53a23b099366c36868ba0383cf0
alpuinschunk <40763575+alpuinschunk@users.noreply.github.com>
2018-07-29 22:31:08 -0300

    Ejercicio 4, corregir

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,37 @@
 function mostrar()
 {
+	var numeroA;
+	var numeroB;
+	var formIngreso;
 
+	numeroA=prompt("Ingrese el primer número:","Número");
+	numeroB=prompt("Ingrese el segundo número:","Número");
+
+	if(numeroA==numeroB)
+	{
+		alert(numeroA+numeroB);
+	}
+
+	else
+	{
+		numeroA=parseInt(numeroA);
+		numeroB=parseInt(numeroB);
+		
+		if(numeroA>numeroB)
+		{
+			alert(numeroA/numeroB);
+		}
+		else
+		{
+			if(numeroA<numeroB)
+			{
+				alert(numeroA+numeroB);
+			}
+			else if ((numeroA+numeroB)<50)
+			{
+				alert(numeroA+numeroB);
+				alert("La suma es "+(numeroA+numeroB)+" y es menor a 50.");
+			}
+		}
+	}
 }
