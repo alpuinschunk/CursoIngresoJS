1b1116ff1677c2f25061c684cc11c23239c8bb42
alpuinschunk <40763575+alpuinschunk@users.noreply.github.com>
2018-07-29 23:05:31 -0300

    Ejercicio 5

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+	var dia=prompt("Ingrese un dia de la semana");
 
+	dia=dia.toLowerCase();
+
+	switch(dia)
+	{
+		case "lunes":
+		case "martes":
+		case "miercoles":
+		case "jueves":
+		case "viernes":
+			alert("¡A trabajar!");
+			break;
+		case "sabado":
+		case "domingo":
+			alert("¡Buen finde!");
+			break;
+		default:
+			alert("Dia no válido");
+	}
 }
