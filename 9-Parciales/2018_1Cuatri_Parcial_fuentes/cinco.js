0df75dbd62edbac3744f95d03d76036a5f34f13b
alpuinschunk <40763575+alpuinschunk@users.noreply.github.com>
2018-07-29 23:04:12 -0300

    Ejercicio 5

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,28 @@
 function mostrar()
 {
+	var planetas;
 
+	planetas=prompt("Ingrese un planeta del sistema solar");
+	planetas=planetas.toLowerCase();
+
+	switch(planetas)
+	{
+		case "mercurio":
+		case "venus":
+			alert("Acá hace más calor");
+			break;
+		case "tierra":
+			alert("Acá vivimos");
+			break
+		case "marte":
+		case "jupiter":
+		case "saturno":
+		case "neptuno":
+		case "urano":
+			alert("Acá hace más frio");
+			break;
+		default:
+			alert("Planeta no válido");
+
+	}
 }
