ca3c3302573fc219ed016f19a80dda2fb0d5b4a6
alpuinschunk <40763575+alpuinschunk@users.noreply.github.com>
2018-07-29 22:37:57 -0300

    Ejercicio 4, corregir

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,36 @@
 function mostrar()
 {
+	var numeroA;
+	var numeroB;
 
+	numeroA=prompt("Ingrese el primer número:","Número");
+	numeroB=prompt("Ingrese el segundo número:","Número");
+
+	if(numeroA==numeroB)
+	{
+		alert(numeroA+numeroB);
+	}
+
+	else
+	{
+		numeroA=parseInt(numeroA);
+		numeroB=parseInt(numeroB);
+		
+		if(numeroA>numeroB)
+		{
+			alert(numeroA-numeroB);
+		}
+		else
+		{
+			if(numeroA<numeroB)
+			{
+				alert(numeroA+numeroB);
+			}
+			else if ((numeroA+numeroB)>10)
+			{
+				alert(numeroA+numeroB);
+				alert("La suma es "+(numeroA+numeroB)+" y es mayor a 10.");
+			}
+		}
+	}
 }
