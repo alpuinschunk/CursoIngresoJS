b401b4d3fb85f4ea9834b3bd0c44af3857151654
alpuinschunk <40763575+alpuinschunk@users.noreply.github.com>
2018-08-05 21:38:05 -0300

    8

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -18,83 +18,92 @@ function mostrar()
 	( b , 3) 
 	( k , 7)*/
 
-	 var letra;
-	 var numero;
-	 var contPar;
-	 var contImpar;
-	 var contCeros;
-	 var promedioPositivos;
-	 var sumaNegativos;	
-	 var respuesta;
-	 var negativos;
-	 var positivos;
-	 var contPositivos;
-	 var numeroMenor;
-	 var numeroMayor;
-	 var letraNumeroMayor;
-	 var letraNumeroMenor;
+	var letra;
+	var numero;
+	var respuesta;
+	var contPares;
+	var contImpares;
+	var contCeros;
+	var positivos;
+	var sumaPositivos;
+	var contPositivos;
+	var negativos;
+	var sumaNegativos;
+	var contador;
 
-	 numeroMayor=100;
-	 numeroMenor=-100;
-	 contPositivos=0;
-	 contador=0;
-	 contPar=0;
-	 contImpar=0;
-	 contCeros=0;
-	 respuesta='si';
+	respuesta='si';
+	contPares=0;
+	contImpares=0;
+	contCeros=0;
+	contPositivos=0;
+	sumaNegativos=0;
+	sumaPositivos=0;
+	contador=0;
 
-	 while(respuesta=='si')
-	 {
-	 	contador++;
-	 	letra=prompt("Ingrese una letra");
-	 	letra=letra.toLowerCase();
 
-	 	numero=parseInt(prompt("Ingrese un numero entre -100 y 100 #"+contador));
 
-	 	while(numero<-100||numero>100);
-	 	{
-	 		numero=parseInt(prompt("Error. Reingrese un numero entre -100 y 100"));
-	 	}
-	 	if(numero%2==0)
-	 	{
-	 		contPar++;
-	 	}
-	 	else 
-	 	{
-	 		contImpar++;
-	 	}
-	 	if(numero==0)
-	 	{
-	 		contCeros++;
-	 	}
-	 	if(numero>0)
-	 	{
-	 		positivos=numero;
-	 		contPositivos++;
-	 	}
-	 	else
-	 	{
-	 		negativos=numero;
-	 		sumaNegativos=negativos+numero;
-	 	}
-	 	if(numero>numeroMayor)
-	 	{
-	 		numeroMayor=numero;
-	 		letraNumeroMayor=letra;
-	 	}
-	 	if(numero<numeroMenor)
-	 	{
-	 		numeroMenor=numero;
-	 		letraNumeroMenor=letra;
-	 	}
-	 	respuesta=prompt("¿Desea seguir ingresando?");
-	 }
-	 promedioPositivos=positivos/contPositivos;
+	while(respuesta=='si')
+	{
+		contador++;
+		letra=prompt("Ingrese una letra");
+		numero=parseInt(prompt("Ingrese un número entre el -100 y 100"));
 
-	document.write("<br> La cantidad de números pares es "+contPar); 
-	document.write("<br> La cantidad de números impares es "+contImpar); 
-	document.write("<br> La cantidad de ceros es "+contCeros); 
-	document.write("<br> El promedio de todos los números positivos ingresados es "+promedioPositivos); 
-	document.write("<br> La suma de todos los números negativos es "+sumaNegativos);
-	document.write("<br> El número y la letra del máximo es"+numeroMayor+" y del minimo es "+numeroMenor);
+		while(numero<-100||numero>100)
+		{
+			numero=parseInt(prompt("Error, reingrese un número entre el -100 y 100"));
+		}
+		if(numero%2==0)
+		{
+			contPares++;
+		}
+		else
+		{
+			contImpares++;
+		}
+		if(numero==0)
+		{
+			contCeros++;
+		}//hasta aca esta bien.
+		if(numero>0)
+		{
+			positivos=numero;
+			sumaPositivos=sumaPositivos+positivos;
+			contPositivos++;
+		}
+		else
+		{
+			negativos=numero;
+			sumaNegativos=sumaNegativos+negativos;
+		}
+		if(contador==1)
+		{
+			numeroMayor=numero;
+			letraNumMayor=letra;
+			numeroMenor=numero;
+			letraNumMenor=letra
+		}
+		else
+		{
+			if (numero>numeroMayor)
+			{
+				numeroMayor=numero;
+				letraNumMayor=letra;
+			}
+			if(numero<numeroMenor)
+			{
+				numeroMenor=numero;
+				letraNumMenor=letra;
+			}
+		}
+		respuesta=prompt("¿Desea continuar? si/no");
+	}
+	promedioPositivos=sumaPositivos/contPositivos;
+
+	document.write("<br>La cantidad de números pares es "+contPares); 
+	document.write("<br>La cantidad de números impares es "+contImpares); 
+	document.write("<br> La cantidad de ceros es "+contCeros);
+	document.write("<br>El promedio de todos los números positivos ingresados es "+promedioPositivos); 
+	document.write("<br>La suma de todos los números negativos es "+sumaNegativos);
+	document.write("<br>El número del máximo es "+numeroMayor+" y su letra es "+letraNumMayor);
+	document.write("<br> El numero mínimo es "+numeroMenor+" y su letra es "+letraNumMenor);
 }
